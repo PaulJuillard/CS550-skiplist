743e9f837c544ae73008589572130061d0be8e82
hugolepeytre <hugo.lepeytre@gmail.com>
2021-11-30 17:57:10 +0100

    Added build, coded "remove"

--- skiplist.scala
+++ skiplist.scala
@@ -1,4 +1,6 @@
 //import stainless.lang._
+import math.max
+import math.min
 
 object SkipList {
   
@@ -9,7 +11,7 @@ object SkipList {
   case class Node[T](root: T, left: Tree[T], right: Tree[T]) extends Tree[T]
   */
   
-  class SkipList(head: Tree[BigInt,BigInt], max_height: Int)
+  case class SkipList(head: Tree[BigInt,BigInt], max_height: Int)
   sealed abstract class Tree[K,V]
   case class SkipNode[K,V](key: K, value: V, down: Tree[K,V], right: Tree[K,V], height: Int) extends Tree[K,V]
 
@@ -18,15 +20,25 @@ object SkipList {
 
   // note: make a head object? might simplify insert
 
+  // TODO : Reread code (Hugo au moins)
+  // TODO : Make sure first element is always +inf, and last element is always +inf (or make Stainless assume, TBD later)
+  // TODO : Add randomness with insert
+  // TODO : Test
+  // TODO : Write invariants and make Stainless prove them
+  // Invariants : If insert element then search, found
+  // If search element in the list, it is found
+  // If remove then search, not found
+  // Every level is a subset of the level below
+  // Search is probabilistically log
+
 
 
 
   def search(sl: SkipList, target: BigInt): Option[BigInt] = {
-
-    def search_bis(t: Tree[BigInt,BigInt], target: BigInt): Option[BigInt] =  t match {
+    def search_bis(t: Tree[BigInt,BigInt], target: BigInt): Option[BigInt] = t match {
       case SkipNode(k,v,d,r,_) =>
           // found
-          if(k  == target){ 
+          if(k  == target) { 
             Some(v) 
           }
           else {
@@ -42,9 +54,11 @@ object SkipList {
                 }
               // might be lower
               case Tail() => search_bis(d, target)
+              case Bottom() => None
             }
           }
       case Bottom() => None //key not in list
+      case Tail() => None
     }
 
     val a = sl.head
@@ -60,6 +74,7 @@ object SkipList {
         if (k == target){t}
         else search_right(right,target)
       case Tail() => t
+      case Bottom() => t
     }
     // operating on level l
     def level_insert(t: Tree[BigInt,BigInt], target: BigInt, value: BigInt, target_height:Int): Tree[BigInt,BigInt] = t match {
@@ -86,7 +101,7 @@ object SkipList {
           case SkipNode(k2,_,_,_,_) => {
             if(k2<target){
               level_insert(right, target, value, target_height)
-            } 
+            }
             else{ // t is pred of target at level l
               if(current_level > height) {
                 level_insert(down, target, value, target_height)
@@ -104,14 +119,15 @@ object SkipList {
                 SkipNode(k, v, new_down, right, current_level)
 
               }
-
           }
         }
+        case Bottom() => t
         
           
         }
       }
       case Tail() => t
+      case Bottom() => t
     }
 
     // If the key to insert is smaller than the current smallest key
@@ -152,6 +168,25 @@ object SkipList {
     val new_tree = insert(sl.head, k, v, height)
     val new_height = max(min(height,sl.max_height+1),sl.max_height)
     new SkipList(new_tree,new_height) //TODO:+1 ?
+  }
+
+  def remove(sl: SkipList, k: BigInt): SkipList = {
+    def remove_(t: Tree[BigInt, BigInt], k: BigInt): Tree[BigInt, BigInt] = t match {
+      case SkipNode(key, value, down, right, height) => {
+        right match {
+          case SkipNode(keyR, valueR, downR, rightR, heightR) => {
+            if (keyR == k) {SkipNode(key, value, remove_(down, k), rightR, height)}
+            else if (keyR < k) {SkipNode(key, value, down, remove_(right, k), height)}
+            else {SkipNode(key, value, remove_(down, k), right, height)}
+          }
+          case Bottom() => t // Cannot happen
+          case Tail() => SkipNode(key, value, remove_(down, k), right, height)
+        }
+      }
+      case Tail() => t // Should not happen
+      case Bottom() => t // Should happen once
+    }
+    SkipList(remove_(sl.head, k), sl.max_height)
   }
 
 }
